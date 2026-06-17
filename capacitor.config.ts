8558513a0ebf171bd46d7332ecf02af607c025be
gitworld08210 <akhilesh.8676@gmail.com>
2026-06-17 18:42:44 +0530

    Refactor capacitor.config.ts to use JSON format

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
 
-const config: CapacitorConfig = {
-  appId: 'com.lifeframe.app',
-  appName: 'Lifeframe',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https'
+{
+  "appId": "com.lifeframe.app",
+  "appName": "Lifeframe",
+  "webDir": "dist",
+  "server": {
+    "androidScheme": "https"
   }
-};
-
-export default config;
+}
